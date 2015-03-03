df1aac9a076a1b1e1c4d30abfb487f516a303891
Andrew Berry <deviantintegral@gmail.com>
2015-03-03 13:36:29 -0500

    Add a NodeAccess exception.

---
+++ src/Plugin/client_error/NodeAccessException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Exception thrown when node_access() fails.
+ */
+
+namespace Drupal\client_error_trace\Plugin\client_error;
+
+/**
+ * Exception thrown when node_access() fails.
+ *
+ * @class NodeAccessException
+ *
+ * @package Drupal\client_error_trace\Plugin\client_error
+ */
+class NodeAccessException extends \Exception {
+
+}
